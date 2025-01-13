dd887bf75d9d50aa8276f949028ab847133edacd
Ricardo lego <hola@legoricardo.com>
2025-01-13 00:42:02 -0600

    Creacion json, sqlinserts

---
+++ Inventarios_Mp.py
@@ -0,0 +1,120 @@
+
+import json
+import math
+from dotenv import load_dotenv
+
+import os 
+
+import pandas as pd
+
+load_dotenv()
+
+class Aplicativo:
+    
+    def __init___( self, Id, DisplayName ):
+        self.Id=Id
+        self.DisplayName = DisplayName
+        
+class AplicativoToinsert():
+    
+    def __init__(self, Aplicativos,  Estatus, Salud, Peso, Factor ):
+        self.Aplicativos = Aplicativos
+        self.Estatus = Estatus
+        self.Salud =  f"{Salud}"
+        self.Peso =  Peso
+        self.Factor =  Factor
+        
+    def Createobj(self):
+        return {
+            "Aplicativos":self.Aplicativos,
+            "Estatus": self.Estatus, 
+            "Salud":self.Salud, 
+            "Peso":self.Peso,
+            "Factor":self.Factor
+        }
+
+
+FileName= os.getenv("FileName")
+
+CatalogJson  = os.getenv("CatlogJson")
+
+_relativePath =  os.path.dirname(os.path.abspath(__file__))
+
+mainPathFile = os.path.join(_relativePath, FileName)
+
+jsonpathfile = os.path.join(_relativePath, CatalogJson)
+
+def readfile():
+    try:
+        return pd.read_excel(mainPathFile)
+    except Exception as e:
+        print(f"Error al leer el archivo excel: {e}")
+        return None
+
+
+def readjson():
+   with open(jsonpathfile, "r", encoding="utf-8") as file:
+        return json.load(file)
+
+jsonfile =  readjson()
+rd =  readfile()
+
+rowstring = ""
+
+
+EstatusNuevos = []
+ 
+def SetEstatus(Id, Estatus):
+    
+
+     EachAplicativo = Aplicativo()
+     EachAplicativo.DisplayName = Estatus
+     EachAplicativo.Id= Id
+     
+     
+     if Id > 7 and  Estatus not in EstatusNuevos:
+        EstatusNuevos.append(Estatus)         
+     
+     return EachAplicativo
+
+
+def SetNewAplicativoJson(Aplicativos, Estatus, Salud, Peso, Factor):
+    return AplicativoToinsert( Aplicativos, Estatus,Salud, Peso, Factor)
+
+    
+
+
+rowstring = ""
+
+AplicativosJson = []
+
+with open( os.path.join(_relativePath,  "resultado.json"), "w", encoding="utf-8") as insertjson:
+    
+
+    
+    for index, row in rd.iterrows():
+        
+        EstatusObj = next(( SetEstatus(item["Id"], item["DisplayName"])  for item in jsonfile if item["DisplayName"] == row["Estatus"] ), SetEstatus(8, "Desarrollo") )
+        
+        Aplicativos = row["Applications"]
+        Estatus = EstatusObj.DisplayName
+        Salud =  0 if math.isnan( row["Salud"] ) else row["Salud"]/100
+        Peso = 0 if row["Peso"]=="TBD" else row["Peso"]
+        Factor =  Peso*Salud 
+        
+   
+        
+        AplicativosJson.append( AplicativoToinsert( Aplicativos, Estatus,Salud, Peso, Factor) )
+        
+        
+        rowstring += f"\nINSERT INTO Aplicativos ([Nombre], [Salud], Peso, Factor, IdEstatusAplicativoFK, Activa, Fecha) Values ('{Aplicativos}','{Salud}',{Peso},{Factor},{EstatusObj.Id},1, CURRENT_TIMESTAMP)"
+        
+    datadict = [ item.Createobj()  for item in AplicativosJson]
+    
+    print(datadict)
+    
+    insertjson.write( json.dumps(datadict, indent=4) ) 
+    
+
+with open( os.path.join(_relativePath,  "insertsql.sql"), "w", encoding="utf-8") as insertsql:
+    insertsql.write( rowstring ) 
